0699823b23a091eb31a674ab78d873de31f6e34b
Sampathkumar Rangadhamaiah <nomail@nodomain.com>
2018-01-03 23:13:06 +0530

    Change:
    pom.xml - Adding dependency for javax.servlet.
    RegistrationServlet - Adding servlet stub.

---
+++ simple-webapp-with-tdd/src/main/java/com/demo/with/tdd/servlet/RegistrationServlet.java
@@ -0,0 +1,12 @@
+package com.demo.with.tdd.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class RegistrationServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
