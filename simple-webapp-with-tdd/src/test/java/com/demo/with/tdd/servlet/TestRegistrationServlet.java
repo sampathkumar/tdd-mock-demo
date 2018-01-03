386e0277056c85459c60da83580589840f9343ef
Sampathkumar Rangadhamaiah <nomail@nodomain.com>
2018-01-03 23:22:57 +0530

    Preparign test class and servlet class.

--- simple-webapp-with-tdd/src/test/java/com/demo/with/tdd/servlet/TestRegistrationServlet.java
+++ simple-webapp-with-tdd/src/test/java/com/demo/with/tdd/servlet/TestRegistrationServlet.java
@@ -1,5 +1,21 @@
 package com.demo.with.tdd.servlet;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestRegistrationServlet {
 
+	/**
+	 * This is Class Under Test
+	 */
+	private RegistrationServlet registrationServlet;
+
+	@Before
+	public void setup() {
+		registrationServlet = new RegistrationServlet();
+	}
+
+	@Test
+	public void validateLastName() {
+	}
 }
