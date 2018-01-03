998eb0cc3e31ceb2ac4f0dc3cce3c00a61d471ea
Sampathkumar Rangadhamaiah <nomail@nodomain.com>
2018-01-03 21:44:21 +0530

    Change: RegistrationServlet.java - Fetching request parameters and printing them on console.

--- simple-webapp/src/main/java/com/demo/tdd/servlet/RegistrationServlet.java
+++ simple-webapp/src/main/java/com/demo/tdd/servlet/RegistrationServlet.java
@@ -15,9 +15,21 @@ public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("RegistrationServlet.doPost()");
+		String userName = request.getParameter("txtUserName");
+		String firstName = request.getParameter("txtFirstName");
+		String lastName = request.getParameter("txtLastName");
+		String dateOfBirth = request.getParameter("txtDateOfBirth");
+		String email = request.getParameter("txtEmail");
+		String phoneNum = request.getParameter("txtPhoneNum");
+
+		System.out.println("RegistrationServlet.doPost(): User Name: " + userName);
+		System.out.println("RegistrationServlet.doPost(): First Name: " + firstName);
+		System.out.println("RegistrationServlet.doPost(): Last Name: " + lastName);
+		System.out.println("RegistrationServlet.doPost(): DOB: " + dateOfBirth);
+		System.out.println("RegistrationServlet.doPost(): Email: " + email);
+		System.out.println("RegistrationServlet.doPost(): Phone Number: " + phoneNum);
 	}
 
 }
