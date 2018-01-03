a00479230ebf2680fd2f51085148571d057a0b83
Sampathkumar Rangadhamaiah <nomail@nodomain.com>
2018-01-03 22:15:13 +0530

    Changes: Sending json response from servlet.
    pom.xml - adding dependency for json.
    RegistrationServlet.java - Sending json response of the received parameters.

--- simple-webapp/src/main/java/com/demo/tdd/servlet/RegistrationServlet.java
+++ simple-webapp/src/main/java/com/demo/tdd/servlet/RegistrationServlet.java
@@ -1,11 +1,14 @@
 package com.demo.tdd.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 
 public class RegistrationServlet extends HttpServlet {
 
@@ -15,7 +18,8 @@ public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("RegistrationServlet.doPost()");
 		String userName = request.getParameter("txtUserName");
 		String firstName = request.getParameter("txtFirstName");
@@ -30,6 +34,20 @@ public class RegistrationServlet extends HttpServlet {
 		System.out.println("RegistrationServlet.doPost(): DOB: " + dateOfBirth);
 		System.out.println("RegistrationServlet.doPost(): Email: " + email);
 		System.out.println("RegistrationServlet.doPost(): Phone Number: " + phoneNum);
+
+		response.setContentType("application/json");
+
+		JSONObject responseJson = new JSONObject();
+
+		responseJson.put("User Name", userName);
+		responseJson.put("First Name", firstName);
+		responseJson.put("Last Name", lastName);
+		responseJson.put("DOB", dateOfBirth);
+		responseJson.put("Email", email);
+		responseJson.put("Phone Number", phoneNum);
+
+		PrintWriter outputWriter = response.getWriter();
+		outputWriter.write(responseJson.toString());
 	}
 
 }
